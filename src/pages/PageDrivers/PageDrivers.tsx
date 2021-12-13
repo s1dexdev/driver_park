c13ad5a3a1c4b4c549b948b8bb67d88679ef2e46
s1dexdev <maksym.se@gmail.com>
2021-12-13 12:38:26 +0200

    added navBar

--- src/pages/PageDrivers/PageDrivers.tsx
+++ src/pages/PageDrivers/PageDrivers.tsx
@@ -1,7 +1,14 @@
+import NavBar from '../../components/NavBar';
+import { ReactComponent as Driver } from '../../images/driver.svg';
 import styles from './PageDrivers.module.scss';
 
 function PageDrivers() {
-    return <h1 className={styles.driversPage}>Drivers Page</h1>;
+    return (
+        <>
+            <NavBar icon={Driver} text="Drivers" />
+            <h1 className={styles.driversPage}>Drivers Page</h1>
+        </>
+    );
 }
 
 export default PageDrivers;
