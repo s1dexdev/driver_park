777debc06903d9f3bf3a70c2a5766e931ab11007
marigeorge007 <maryofgeorge@gmail.com>
2021-12-14 18:19:50 -0500

    first commit for table cars

--- src/pages/PageCars/PageCars.tsx
+++ src/pages/PageCars/PageCars.tsx
@@ -1,7 +1,9 @@
 import NavBar from '../../components/NavBar';
 import { ReactComponent as Car } from '../../images/car.svg';
 import navConfig from '../../utils/constants/navConfig';
+import { tableCarsColumns, statusCars } from '../../utils/constants/index';
 import styles from './PageCars.module.scss';
+import React from 'react';
 
 function PageCars(): JSX.Element {
     const { cars } = navConfig;
@@ -9,7 +11,24 @@ function PageCars(): JSX.Element {
     return (
         <>
             <NavBar icon={Car} text={cars.label} />
-            <h1 className={styles.carsPage}>Cars Page</h1>
+            {/* <h1 className={styles.carsPage}>Cars Page</h1> */}
+
+            <div className={styles.blockCars}>
+                {tableCarsColumns.map(item => {
+                    if (item !== 'Standard') {
+                        <div key={item}>{item}</div>;
+                    } else {
+                        <>
+                            <input list="cars" />
+                            <datalist id="cars">
+                                {statusCars.map(data => {
+                                    <option>{data}</option>;
+                                })}
+                            </datalist>
+                        </>;
+                    }
+                })}
+            </div>
         </>
     );
 }
