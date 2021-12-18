91b180bf2b827af4f484e70900f4bf89b94f33e0
s1dexdev <maksym.se@gmail.com>
2021-12-18 18:30:33 +0200

    added redux

--- src/pages/PageDrivers/PageDrivers.tsx
+++ src/pages/PageDrivers/PageDrivers.tsx
@@ -1,9 +1,3 @@
-import { useDispatch, useSelector } from 'react-redux';
-import {
-    FETCH_DRIVERS_REQUEST,
-    fetchDriversRequest,
-} from '../../redux/drivers/actions';
-import { driversSelector } from '../../redux/drivers/selectors';
 import NavBar from '../../components/NavBar';
 import { ReactComponent as Driver } from '../../images/driver.svg';
 import navConfig from '../../utils/constants/navConfig';
@@ -12,16 +6,10 @@ import styles from './PageDrivers.module.scss';
 function PageDrivers(): JSX.Element {
     const { drivers } = navConfig;
 
-    const dispatch = useDispatch();
-    const items = useSelector(driversSelector);
-
     return (
         <>
             <NavBar icon={Driver} text={drivers.label} />
             <h1 className={styles.driversPage}>Drivers Page</h1>
-            <button onClick={() => dispatch(fetchDriversRequest())}>
-                drivers
-            </button>
         </>
     );
 }
