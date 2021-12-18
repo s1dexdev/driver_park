9740cc37e5dc54aa7daeb0f21a28e74cc3043718
s1dexdev <maksym.se@gmail.com>
2021-12-18 15:53:23 +0200

    remove files

--- src/components/Container/Container.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './Container.module.scss';
-
-function Container({ children }: { children: React.ReactNode }): JSX.Element {
-    return <div className={styles.container}>{children}</div>;
-}
-
-export default Container;
