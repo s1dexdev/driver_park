0dab0b405ed1f5309c2a824ec74089456734e6fb
Natali-Nazarenko <91319785+Natali-Nazarenko@users.noreply.github.com>
2021-12-29 17:04:50 +0200

    added color theme to 'Choise Status' button

--- src/components/TableCars/TableHeaderCars/index.tsx
+++ src/components/TableCars/TableHeaderCars/index.tsx
@@ -1,9 +1,17 @@
 import { carsConfig } from '../../../utils/constants';
 import { concatClasses } from '../../../helpers';
+import { useState } from 'react';
 import { ReactComponent as SortDown } from '../../../images/sortDown.svg';
+import { ReactComponent as SortUp } from '../../../images/sortUp.svg';
+
 import styles from './TableHeaderCars.module.scss';
 
 export function TableHeaderCars(): JSX.Element {
+    const [sortData, setSortData] = useState(true);
+
+    const choiseSort = () => {
+        setSortData(!sortData);
+    };
     return (
         <ul className={styles.tableHeader}>
             <li key={'checkbox'} className={`${styles.tableHeader__item}`}>
@@ -12,9 +20,18 @@ export function TableHeaderCars(): JSX.Element {
             {carsConfig.map(({ title, classes }) => (
                 <li key={title} className={concatClasses(styles, classes)}>
                     {title}
-                    {title !== 'ACTIONS' && (
-                        <SortDown className={styles.tableHeader__icon} />
-                    )}
+                    {title !== 'ACTIONS' &&
+                        (sortData ? (
+                            <SortDown
+                                className={styles.tableHeader__icon}
+                                onClick={choiseSort}
+                            />
+                        ) : (
+                            <SortUp
+                                className={styles.tableHeader__icon}
+                                onClick={choiseSort}
+                            />
+                        ))}
                 </li>
             ))}
         </ul>
