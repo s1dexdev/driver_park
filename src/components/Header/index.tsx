1cb7332dee9019742a6528111b399b38315f8a0f
s1dexdev <maksym.se@gmail.com>
2022-01-02 14:12:07 +0200

    added buttons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import { Container, Menu, UserInfo } from '../';
 import { ReactComponent as IconMenu } from '../../images/menu.svg';
 import { ReactComponent as IconLogo } from '../../images/logo.svg';
+import { ReactComponent as Rus } from '../../lang/icons/ru.svg';
+import { ReactComponent as Eng } from '../../lang/icons/gb.svg';
 import styles from './Header.module.scss';
 
 export function Header(): JSX.Element {
@@ -25,7 +27,23 @@ export function Header(): JSX.Element {
                             <IconLogo />
                         </NavLink>
                     </div>
-                    <UserInfo />
+                    <div className={styles.header__infoWrapper}>
+                        <UserInfo />
+                        <div className={styles.header__langBox}>
+                            <button
+                                type="button"
+                                className={styles.header__langBtn}
+                            >
+                                <Eng width={24} />
+                            </button>
+                            <button
+                                type="button"
+                                className={styles.header__langBtn}
+                            >
+                                <Rus width={24} />
+                            </button>
+                        </div>
+                    </div>
                 </div>
                 {menu && <Menu onShowMenu={setMenu} />}
             </Container>
--- src/components/TableCars/index.tsx
+++ src/components/TableCars/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { Button, CarForm, Modal } from '../';
 import { carsSelector } from '../../redux/cars/selectors';
 import { TableHeaderCars } from './TableHeaderCars';
 import { ListCars } from './ListCars';
@@ -6,29 +9,53 @@ import styles from './TableCars.module.scss';
 
 export function TableCars(): JSX.Element {
     const cars = useSelector(carsSelector);
+    const { driverId } = useParams();
+    const [formType, setFormType] = useState(false);
+    const [modalActive, setModalActive] = useState(false);
+
+    const renderModalCar = () => {
+        setFormType(true);
+        setModalActive(true);
+    };
 
     return (
-        <div className={styles.carTable}>
-            <p className={styles.carTable__title}>All cars</p>
-            <div className={styles.carTable__wrapper}>
-                <div className={styles.carTable__wrapperCars}>
-                    <p className={styles.carTable__numberCars}>
-                        {' '}
-                        All cars{' '}
-                        <span className={styles.carTable__numberCars_color}>
-                            ({cars.length})
-                        </span>
-                    </p>
+        <>
+            <div className={styles.carTable}>
+                <p className={styles.carTable__title}>All cars</p>
+                <div className={styles.carTable__wrapper}>
+                    <div className={styles.carTable__wrapperCars}>
+                        <p className={styles.carTable__numberCars}>
+                            {' '}
+                            All cars{' '}
+                            <span className={styles.carTable__numberCars_color}>
+                                ({cars.length})
+                            </span>
+                        </p>
+                    </div>
+                    <TableHeaderCars />
+                    {cars.length ? (
+                        <ListCars />
+                    ) : (
+                        <>
+                            <div className={styles.info}>
+                                <p className={styles.info__text}>
+                                    Oops, no one car found...
+                                </p>
+                                {driverId && (
+                                    <Button
+                                        text="Add car"
+                                        className={styles.info__addBtn}
+                                        onClick={() => renderModalCar()}
+                                    />
+                                )}
+                            </div>
+                        </>
+                    )}
                 </div>
-                <TableHeaderCars />
-                {cars.length ? (
-                    <ListCars />
-                ) : (
-                    <p className={styles.carTable__info}>
-                        Oops, no one car found...
-                    </p>
-                )}
             </div>
-        </div>
+            <Modal active={modalActive} setActive={setModalActive}>
+                {formType && <CarForm id={Number(driverId)} />}
+            </Modal>
+        </>
     );
 }
